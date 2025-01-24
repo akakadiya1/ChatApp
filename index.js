447a9d1ba422507372261c424c11351979667009
akakadiya1 <akakadiya1@myseneca.ca>
2025-01-24 18:18:24 -0500

    Integrate Socket.io to Chat Application

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const http = require("http");
 const express = require("express");
 const path = require("path");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server);
+
+// Socket.io 
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
 
 app.use(express.static(path.resolve(__dirname, "public")));
 
